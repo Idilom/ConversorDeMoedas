819a1671b495a890bfc43e9c0ae0428507805ccb
Idilom <idilomdev@gmail.com>
2025-05-14 14:57:28 -0300

    First Commit

---
+++ src/main/java/com/idilom/currencyconverter/CurrencyConverterApplication.java
@@ -0,0 +1,13 @@
+package com.idilom.currencyconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConverterApplication.class, args);
+	}
+ public String Welcome = "Hello World";
+}
